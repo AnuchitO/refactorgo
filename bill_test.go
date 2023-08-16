bc1e93a53c665d5a185544dd9b7fdf95bab32663
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-08-16 10:11:31 +0700

    add

--- bill_test.go
+++ bill_test.go
@@ -32,3 +32,37 @@ you earned 47 credits
 		t.Errorf("expect %q but got %q", exp, bill)
 	}
 }
+
+func TestPrintBillAsHtml(t *testing.T) {
+	t.Skip("skip html test")
+	inv := Invoice{
+		Customer: "Bigco",
+		Performances: []Performance{
+			{PlayID: "hamlet", Audience: 55},
+			{PlayID: "as-like", Audience: 35},
+			{PlayID: "othello", Audience: 40},
+		}}
+	plays := map[string]map[string]string{
+		"hamlet":  {"name": "Hamlet", "type": "tragedy"},
+		"as-like": {"name": "As You Like It", "type": "comedy"},
+		"othello": {"name": "Othello", "type": "tragedy"},
+	}
+
+	bill := statement(inv, plays)
+
+	exp := `<h1>Statement for Bigco</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>
+<tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+<tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+<tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1730.00</em></p>
+<p>you earned <em>47</em> credits</p>
+`
+
+	if exp != bill {
+		t.Errorf("expect %q but got %q", exp, bill)
+	}
+
+}
